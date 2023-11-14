501b3be4a28e4c1ae7300d681e6eed935f549291
rivemont <william.11bennett@gmail.com>
2023-11-14 13:20:01 -0500

    New plots nad plans.

--- MIVI.R
+++ MIVI.R
@@ -1,10 +1,46 @@
-library(ggplot2)
+# set lwd
+setwd("~/Documents/MIVI/")
 
-mivi_young <- read.csv("./MIVI-YOUNG.csv")
-mivi_flowering <- read.csv("./MIVI-FLOWERING.csv")
-mivi_fruiting <- read.csv("./MIVI-FRUITING.csv")
+library(ggplot2)
+library(dplyr)
+
+# download all Mv: id, observed_on, lat, lon, state, country
+# download no sign 21: id; add stage
+# download flowering 15: id; add stage
+# download fruiting 14: id; add stage
+# join each based on id
+
+
+# quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=21
+mivi_young <- read.csv("./MIVI-YOUNG.csv") %>%
+    select(-place_country_name) %>%
+    mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Vegetation")
+
+mivi_flowering <- read.csv("./MIVI-FLOWERING.csv") %>%
+    mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Flowering")
+
+# quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=14
+mivi_fruiting <- read.csv("./MIVI-FRUITING.csv") %>%
+    mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Seeding")
 
 ggplot(mivi_young, aes(x=longitude,y=latitude)) + geom_point()
 ggplot(mivi_young, aes(x=longitude,y=latitude,color=place_state_name)) + geom_point()
 
 ggplot(mivi_young, aes(x=latitude)) + geom_histogram(binwidth=0.2)
+
+
+mivi_all <- rbind(mivi_young, mivi_flowering, mivi_fruiting)
+
+timeclip <- c(as.Date("2014-01-01"), as.Date("2023-12-30"))
+
+pdf(file="output1.pdf")
+
+# Time series by latitude, color by stage
+ggplot(mivi_all, aes(date, latitude)) + geom_point(aes(color=stage)) +
+  scale_x_date(limits=timeclip, date_breaks = "1 year", date_labels = "%Y") +
+  labs(title="Observations by latitude over time")
+
+# Time series within one year by latitude
+
+dev.off()
+
