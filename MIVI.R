c4466d7bba23ae7468d97ea531f544e0f5ffdb5c
rivermont <william.11bennett@gmail.com>
2023-11-21 23:42:48 -0500

    New combined data structure

--- MIVI.R
+++ MIVI.R
@@ -1,35 +1,61 @@
 # set lwd
-setwd("~/Documents/MIVI/")
+setwd("~/Downloads/MIVI/")
 
 library(ggplot2)
 library(dplyr)
 
+# term_value=15 is flower budding
+
 # download all Mv: id, observed_on, lat, lon, state, country
+# quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true
+mivi_all <- read.csv("./MIVI-ALL.csv") %>%
+    mutate(date=as.Date(observed_on, format="%Y-%m-%d"))
+
 # download no sign 21: id; add stage
+# quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=21
+mivi_young <- read.csv("MIVI-YOUNG.csv") %>%
+    mutate(state="Vegetation")
+
 # download flowering 15: id; add stage
+# quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=13
+mivi_flowering <- read.csv("./MIVI-FLOWERING.csv") %>%
+  mutate(stage="Flowering")
+
 # download fruiting 14: id; add stage
+# quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=14
+mivi_fruiting <- read.csv("./MIVI-FRUITING.csv") %>%
+  mutate(stage="Seeding")
+
 # join each based on id
+mivi_all %>% inner_join(mivi_young, c("id", "id"))
+mivi_all %>% inner_join(mivi_flowering, c("id", "id"))
+mivi_all %>% inner_join(mivi_fruiting, c("id", "id"))
+
+
+
+
+
 
 
 # quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=21
-mivi_young <- read.csv("./MIVI-YOUNG.csv") %>%
-    select(-place_country_name) %>%
-    mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Vegetation")
+# mivi_young <- read.csv("./MIVI-YOUNG.csv") %>%
+#     select(-place_country_name) %>%
+#     mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Vegetation")
 
-mivi_flowering <- read.csv("./MIVI-FLOWERING.csv") %>%
-    mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Flowering")
+# mivi_flowering <- read.csv("./MIVI-FLOWERING.csv") %>%
+#     mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Flowering")
 
 # quality_grade=research&identifications=any&place_id=97394&taxon_id=116710&verifiable=true&term_id=12&term_value_id=14
-mivi_fruiting <- read.csv("./MIVI-FRUITING.csv") %>%
-    mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Seeding")
+# mivi_fruiting <- read.csv("./MIVI-FRUITING.csv") %>%
+#     mutate(date = as.Date(observed_on, format="%Y-%m-%d"), stage="Seeding")
 
-ggplot(mivi_young, aes(x=longitude,y=latitude)) + geom_point()
-ggplot(mivi_young, aes(x=longitude,y=latitude,color=place_state_name)) + geom_point()
+# ggplot(mivi_young, aes(x=longitude,y=latitude)) + geom_point()
+# ggplot(mivi_young, aes(x=longitude,y=latitude,color=place_state_name)) + geom_point()
 
-ggplot(mivi_young, aes(x=latitude)) + geom_histogram(binwidth=0.2)
+# ggplot(mivi_young, aes(x=latitude)) + geom_histogram(binwidth=0.2)
 
 
-mivi_all <- rbind(mivi_young, mivi_flowering, mivi_fruiting)
+# mivi_all <- rbind(mivi_young, mivi_flowering, mivi_fruiting)
 
 timeclip <- c(as.Date("2014-01-01"), as.Date("2023-12-30"))
 
